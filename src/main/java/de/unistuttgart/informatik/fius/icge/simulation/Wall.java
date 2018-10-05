29ad23e8feabd4f6f67bfbae2b79ea34985fce22
Tobias Wältken <tobias.w@ltken.de>
2018-10-05 17:03:41 +0200

    Remove the ability to spawn walls on none empty cells

--- src/main/java/de/unistuttgart/informatik/fius/icge/simulation/Wall.java
+++ src/main/java/de/unistuttgart/informatik/fius/icge/simulation/Wall.java
@@ -7,10 +7,25 @@
 
 package de.unistuttgart.informatik.fius.icge.simulation;
 
+import de.unistuttgart.informatik.fius.icge.territory.WorldObject.Direction;
 import de.unistuttgart.informatik.fius.icge.territory.WorldObject.Sprite;
 
 public class Wall extends Entity {
     public Wall(Simulation sim) {
         super(sim, Sprite.WALL);
+    }
+    
+    /**
+     * @see de.unistuttgart.informatik.fius.icge.simulation.Entity#spawnInternal(int, int, de.unistuttgart.informatik.fius.icge.territory.WorldObject.Direction)
+     */
+    @Override
+    protected void spawnInternal(int column, int row, Direction direction) throws EntityAlreadyAlive, CellBlockedByWall {
+        for(Entity e : this.simulation().entitiesWith(row, column))
+            throw new CellNotEmpty();
+        
+        super.spawnInternal(column, row, direction);
+    }
+    
+    public static class CellNotEmpty extends RuntimeException {
     }
 }
