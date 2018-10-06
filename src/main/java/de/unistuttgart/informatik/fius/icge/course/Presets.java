56c59a8ae7e37510801a50bcaca0859f6afa2ba3
Tobias Wältken <tobias.w@ltken.de>
2018-10-07 01:04:35 +0200

    Add the ability to pase on editors through the preset functions

--- src/main/java/de/unistuttgart/informatik/fius/icge/course/Presets.java
+++ src/main/java/de/unistuttgart/informatik/fius/icge/course/Presets.java
@@ -11,11 +11,31 @@ import de.unistuttgart.informatik.fius.icge.simulation.EntityType;
 import de.unistuttgart.informatik.fius.icge.territory.Editor;
 import de.unistuttgart.informatik.fius.icge.territory.Territory;
 
+/**
+ * Class containing a few functions to easily add some structures to a Editor.
+ */
 public class Presets {
+    
+    /**
+     * Function to generate a cage around your playing field
+     * 
+     * @param width inner width of the cage
+     * @param height inner height of the cage
+     * @return returns a {@link de.unistuttgart.informatik.fius.icge.territory.Editor}
+     */
     public static Editor cage(int width, int height) {
-
-        Editor ed = new Editor(new Territory());
-
+        return cage(new Editor(new Territory()), width, height);
+    }
+    
+    /**
+     * Function to generate a cage around your playing field
+     * 
+     * @param ed a existing editor to add the cage to
+     * @param width inner width of the cage
+     * @param height inner height of the cage
+     * @return returns a {@link de.unistuttgart.informatik.fius.icge.territory.Editor}
+     */
+    public static Editor cage(Editor ed, int width, int height) {
         for (int x = -1; x <= width; ++x) {
             ed.add(EntityType.WALL, x, -1);
             ed.add(EntityType.WALL, x, height);
@@ -36,10 +56,25 @@ public class Presets {
      * And 1 upwords indicates the number of coins in that field.
      *  
      * @param map the array containing a predefined map
-     * @return returns an Editor to add further work on a map
+     * @return returns a {@link de.unistuttgart.informatik.fius.icge.territory.Editor}
      */
     public static Editor loadFromArray(int[][] map) {
-        return loadFromArray(map, -1, -1);
+        return loadFromArray(new Editor(new Territory()), map, -1, -1);
+    }
+    
+    /**
+     * This function generates an Editor with a array defined map with coins.
+     * A -1 in a Cell indicates a wall
+     * A 0 indicates nothing
+     * And 1 upwords indicates the number of coins in that field.
+     * 
+     * @param map the array containing a predefined map
+     * @param offsetX a X offset applied to the final map
+     * @param offsetY a Y offset applied to the final map
+     * @return returns a {@link de.unistuttgart.informatik.fius.icge.territory.Editor}
+     */
+    public static Editor loadFromArray(int[][] map, int offsetX, int offsetY) {
+        return loadFromArray(new Editor(new Territory()), map, offsetX, offsetY);
     }
     
     /**
@@ -48,14 +83,27 @@ public class Presets {
      * A 0 indicates nothing
      * And 1 upwords indicates the number of coins in that field.
      *  
+     * @param ed a existing editor to add the cage to
+     * @param map the array containing a predefined map
+     * @return returns a {@link de.unistuttgart.informatik.fius.icge.territory.Editor}
+     */
+    public static Editor loadFromArray(Editor ed, int[][] map) {
+        return loadFromArray(ed, map, -1, -1);
+    }
+    
+    /**
+     * This function generates an Editor with a array defined map with coins.
+     * A -1 in a Cell indicates a wall
+     * A 0 indicates nothing
+     * And 1 upwords indicates the number of coins in that field.
+     *  
+     * @param ed a existing editor to add the cage to
      * @param map the array containing a predefined map
      * @param offsetX a X offset applied to the final map
      * @param offsetY a Y offset applied to the final map
-     * @return returns an Editor to add further work on a map
+     * @return returns a {@link de.unistuttgart.informatik.fius.icge.territory.Editor}
      */
-    public static Editor loadFromArray(int[][] map, int offsetX, int offsetY) {
-        Editor ed = new Editor(new Territory());
-        
+    public static Editor loadFromArray(Editor ed, int[][] map, int offsetX, int offsetY) {
         for (int y = 0; y < map.length; y++) {
             for (int x = 0; x < map[y].length; x++) {
                 if (map[y][x] == -1) {
