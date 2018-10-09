7105137b8365088c26e2a100348022f9c940b6fc
haslersn <sebastian.hasler@gmx.net>
2018-10-09 20:30:17 +0200

    event/EventDispatcher, simulation/Simulation: [bugfix] #65 race condition
    Closes #65

--- src/main/java/de/unistuttgart/informatik/fius/icge/event/EventDispatcher.java
+++ src/main/java/de/unistuttgart/informatik/fius/icge/event/EventDispatcher.java
@@ -11,28 +11,39 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 public class EventDispatcher {
-    
+
     private static ArrayList<Listening> _listenings = new ArrayList<>();
     private static ArrayDeque<Runnable> _afterwards = new ArrayDeque<>();
     private static int _raiseRecursionDepth = 0;
-    
+
     public static synchronized EventListener addListener(Class<?> listensFor, EventListener listener) {
         if (!Event.class.isAssignableFrom(listensFor)) throw new IllegalArgumentException();
         _listenings.add(new Listening(listensFor, listener));
         return listener;
     }
-    
+
     public static synchronized boolean removeListener(EventListener listener) {
         return _listenings.removeIf(entry -> entry.listener == listener);
     }
-    
+
     public static synchronized void raise(Event e) throws RaiseAlreadyActive {
-        if (_raiseRecursionDepth != 0) {
+        EventDispatcher.raise(e, () -> {});
+    }
+
+    /**
+     * Raises an event, then runs an `afterTask` and finally runs the
+     * 
+     * @param e
+     * @param afterTask
+     * @throws RaiseAlreadyActive
+     */
+    public static synchronized void raise(Event e, Runnable afterTask) throws RaiseAlreadyActive {
+        if (EventDispatcher._raiseRecursionDepth != 0) {
             throw new RaiseAlreadyActive(); // recursive raise is not supported for now
         }
 
         // actual event hadling
-        ++_raiseRecursionDepth;
+        ++EventDispatcher._raiseRecursionDepth;
         try {
             for (Listening entry : new ArrayList<>(_listenings)) { // new list cause it might get modified concurrently
                 if (entry.listensFor.isAssignableFrom(e.getClass())) {
@@ -41,12 +52,12 @@ public class EventDispatcher {
                     }
                 }
             }
-        }
-        finally {
-            --_raiseRecursionDepth;
+        } finally {
+            --EventDispatcher._raiseRecursionDepth;
         }
 
-        // Call runnables that have been scheduled to be called after the event handling
+        // First run the passed `afterTask` and then the tasks that have been scheduled via `EventDispatcher.afterwards()`
+        afterTask.run();
         while (!_afterwards.isEmpty()) {
             _afterwards.pop().run();
         }
@@ -57,19 +68,24 @@ public class EventDispatcher {
      * If an `EventHandler.raise()` call is currently active (i.e. an event is currently handled)
      * AND this method is called from the same thread where that event is raised (and handled) in,
      * the runnable is run after the handling of that event.
+     * 
      * @param rn
      *            The runnable to schedule
      */
     public static synchronized void afterwards(Runnable rn) {
+        if (EventDispatcher._raiseRecursionDepth == 0) {
+            throw new RaiseNotActive(); // must have an active raise to schedule via `afterwards()`
+        }
+
         _afterwards.add(rn);
     }
-    
+
     private static class Listening {
         public Listening(Class<?> listensFor, EventListener listener) {
             this.listensFor = listensFor;
             this.listener = listener;
         }
-        
+
         public final Class<?> listensFor;
         public final EventListener listener;
     }
@@ -78,6 +94,10 @@ public class EventDispatcher {
 
     public static class RaiseAlreadyActive extends RuntimeException {
         private static final long serialVersionUID = 7713141366627046771L;
+    }
+
+    public static class RaiseNotActive extends RuntimeException {
+        private static final long serialVersionUID = 2178481422042432110L;
     }
 
 }
