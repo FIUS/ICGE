f0dd404c7198ab909e0c29950a2641495cb3f87c
fabianhick <kontakt@fabian-hick.de>
2018-10-11 15:59:54 +0200

    workbench/swing/ToolBar: Add the speed slider
    Closes #32
    Closes #81

--- src/main/java/de/unistuttgart/informatik/fius/icge/workbench/swing/ToolBar.java
+++ src/main/java/de/unistuttgart/informatik/fius/icge/workbench/swing/ToolBar.java
@@ -12,16 +12,20 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JSlider;
 import javax.swing.JToolBar;
 
+import de.unistuttgart.informatik.fius.icge.event.EventDispatcher;
+import de.unistuttgart.informatik.fius.icge.simulation.Simulation;
+import de.unistuttgart.informatik.fius.icge.workbench.Workbench.SetSimulationEvent;
 import de.unistuttgart.informatik.fius.icge.workbench.WorkbenchView;
 import de.unistuttgart.informatik.fius.icge.workbench.tools.SimulationController;
 import de.unistuttgart.informatik.fius.icge.workbench.tools.ToolHandler;
 
 public class ToolBar extends JToolBar {
     private static final long serialVersionUID = 224051849272358057L;
-    
-    private final SwingView _view;
+
+    private final WorkbenchView _view;
     private final JComboBox<String> _dropDown = new JComboBox<>();
     private final ArrayList<Runnable> _dropDownActions = new ArrayList<>();
 
@@ -43,22 +47,23 @@ public class ToolBar extends JToolBar {
         SimulationController sc = new SimulationController(view);
         this.add(sc.playButton(), -1);
         this.add(sc.stepButton(), -1);
+        this.add(this.createSpeedSlider(), -1);
     }
 
     public WorkbenchView view() {
         return this._view;
     }
-    
+
     public void setDropDownToolTip(String text) {
         this._dropDown.setToolTipText(text);
     }
-    
+
     public void addDropDownAction(String name, Runnable callback) {
         this._dropDown.setVisible(true);
         this._dropDown.addItem(name);
         this._dropDownActions.add(callback);
     }
-    
+
     /**
      * Adds a button to this toolbar.
      * 
@@ -69,5 +74,38 @@ public class ToolBar extends JToolBar {
      */
     public void addButton(JButton button, int position) {
         this.add(button, position);
+    }
+
+    private JSlider createSpeedSlider() {
+        JSlider speedSlider = new JSlider(JSlider.HORIZONTAL, 0, 50, 10);
+
+        speedSlider.setMajorTickSpacing(10);
+        speedSlider.setMinorTickSpacing(1);
+        speedSlider.setSnapToTicks(false);
+        speedSlider.setPaintTicks(true);
+        speedSlider.setPaintLabels(true);
+        speedSlider.setInverted(true);
+
+        Runnable updater = () -> {
+            Simulation sim = this._view.simulation();
+            if (sim != null) {
+                sim.setTickMillis(Math.max(1, speedSlider.getValue()));
+            }
+        };
+
+        speedSlider.addChangeListener(e -> {
+            if (!speedSlider.getValueIsAdjusting()) {
+                updater.run();
+            }
+        });
+
+        EventDispatcher.addListener(SetSimulationEvent.class, ev -> {
+            if (this._view == ((SetSimulationEvent) ev).view) {
+                updater.run();
+            }
+            return true;
+        });
+
+        return speedSlider;
     }
 }
